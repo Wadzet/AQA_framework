84e7664974b1cb2568c783f4eaa389e4a7dc982d
Vadym <vadymzastavniy@gmail.com>
2024-08-26 17:20:13 +0300

    complete task 2

--- src/main/java/task_2/Main.java
+++ src/main/java/task_2/Main.java
@@ -2,6 +2,16 @@ package task_2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("For loop:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nWhile loop:");
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
